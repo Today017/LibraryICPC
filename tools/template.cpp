0d78409379c2a7aec99cdce8a6128437a6c63896
Today <82440871+Today017@users.noreply.github.com>
2024-06-29 18:11:57 +0900

    Update and rename template.hpp to template.cpp

--- tools/template.cpp
+++ tools/template.cpp
@@ -22,3 +22,4 @@ template <typename T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 
 template <typename T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
 
 // --------------------------------------------------------
+
